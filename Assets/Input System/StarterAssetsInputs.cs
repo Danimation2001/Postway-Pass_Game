28b75ee46560818a29623659dbc65bc89d40022d
Daniel <dsquires.personal@outlook.com>
2022-03-23 21:37:09 +0800

    Switched to a more robust and feature complete controller.
    Using a starter asset for the controller as it has pretty much everything we need. Can add onto it as needed. Can replace art assets with our own and tweak settings to suit our needs.

---
+++ Assets/Input System/StarterAssetsInputs.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+#if !UNITY_IOS || !UNITY_ANDROID
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+#endif
+
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+#else
+	// old input sys if we do decide to have it (most likely wont)...
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		} 
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+#if !UNITY_IOS || !UNITY_ANDROID
+
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+
+#endif
+
+	}
+	
+}
