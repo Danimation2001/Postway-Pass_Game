ecff60077d7b7a6de59ed4e423e6a0632d810b4a
Daniel <dsquires.personal@outlook.com>
2022-04-12 15:05:49 +0800

    Set up enemy behaviour in the overworld.
    The enemy wanders around a specified area and chases the player when they get too close. If the player leaves the detection range then the enemy goes back to wandering. Plan is to begin combat when either the player and enemy touch or get too close to each other.

---
+++ Assets/Scripts/Overworld/EnemyController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    Transform _target; // the player
+    public float detectionRadius; // how close the player needs to be to be detected
+    NavMeshAgent _agent; // navmeshagent component
+    public float wanderSpeed = 1f; // how fast the enemy moves whilst wandering
+    float _chaseSpeed; // how fast the enemy moves whilst chasing. use speed on agent component to change
+    public Transform wanderDestination; // position to move towards when wandering
+    public float wanderRadius; // how far the enemy can wander away from
+    public float wanderTimer; // time it takes to choose a new destination
+    float _timer;
+    Vector3 _wanderArea; // origin of wandering area.
+
+    void OnEnable() // initialize variables
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _timer = wanderTimer;
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        wanderDestination.position = transform.position;
+        _chaseSpeed = _agent.speed;
+        _wanderArea = transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _distance = Vector3.Distance(_target.position, transform.position); // distance between enemy and player
+        _timer += Time.deltaTime; // increase timer
+
+        if (_distance > detectionRadius) // if the player isn't within detection range
+        {
+            _agent.speed = wanderSpeed; // set the speed to wandering speed
+
+            if (_timer >= wanderTimer) // if it has been long enough to choose a new wander destination
+            {
+                StartCoroutine(Wander());
+                _timer = 0; // reset timer
+            }
+        }
+
+        if (_distance <= detectionRadius) // if the player is within detection range
+        {
+            _agent.speed = _chaseSpeed; // set speed to chase speed
+            _agent.SetDestination(_target.position); // chase the player
+        }
+    }
+
+    IEnumerator Wander() // move to a random wander position
+    {
+        wanderDestination.position = RandomNavSphere(_wanderArea, wanderRadius, -1); // choose random position within a sphere radius of the wander area
+        _agent.SetDestination(wanderDestination.position); // move towards the chosen position
+        yield return new WaitForSeconds(2f); // wait
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layermask) // setup a sphere to wander within that gets a random point within the radius on the navmesh
+    {
+        Vector3 randDirection = Random.insideUnitSphere * distance;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, distance, layermask);
+
+        return navHit.position;
+    }
+
+    void OnDrawGizmosSelected() // lets us see the detection radius in the editor
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
