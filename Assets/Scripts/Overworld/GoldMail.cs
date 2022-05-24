f5911979c52b26f4487bf755c335996329c6a6a4
tillyjowle <101618668+tillyjowle@users.noreply.github.com>
2022-05-24 12:41:26 +0800

    Revert "cleaning files"
    This reverts commit 1dff7c366d8896087eb2755bc90500fad1b0e727.

--- Assets/Scripts/Overworld/GoldMail.cs
+++ Assets/Scripts/Overworld/GoldMail.cs
@@ -1,61 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
 
 public class GoldMail : MonoBehaviour
 {
-    public GameObject UiObject;
-    public TMP_Text messageText;
-    public Animator anim;
-    public int mailID;
-    public TextAsset message;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        foreach (int id in GameManager.Instance.collectedGoldMail) // check if this mail is marked as been collected
-        {
-            if (id == mailID)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-        anim = GetComponent<Animator>();
-    }
-
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            Time.timeScale = 0f;
-            UiObject.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
-
-            if (message != null)
-            {
-                messageText.text = message.text;
-            }
-        }
-    }
-
-    public void Collect()
-    {
-        Time.timeScale = 1f;
-        Cursor.lockState = CursorLockMode.Locked;
-        StartCoroutine(CollectGoldMail());
-    }
-
-    IEnumerator CollectGoldMail()
-    {
-        GameManager.Instance.goldMailCount++;
-        GameManager.Instance.collectedGoldMail.Add(mailID);
-        anim.Play("Collect");
-        yield return new WaitForSeconds(1f);
-        Destroy(gameObject);
-    }
 }
+// public GameObject GoldMail;
+// public GameObject buttonAndTextDrawer;
+
+
+// public void OnCollisionEnter(collision col) {
+//     if(col.gameObject == GoldMail){
+//         buttonAndTextDrawer.SetActive(true);
+//     }
+// }
+// }
+
+// {
+//     public int goldmailID;
+//     Animator _anim;
+
+//     void Start()
+//     {
+//         foreach (int id in GameManager.Instance.collectedMail) // check if this mail is marked as been collected
+//         {
+//             if (id == goldmailID)
+//             {
+//                 gameObject.SetActive(false);
+//             }
+//         }
+//         _anim = GetComponent<Animator>();
+//     }
+
+//     IEnumerator CollectMail()
+//     {
+//         GameManager.Instance.mailCount++;
+//         GameManager.Instance.collectedMail.Add(goldmailID);
+//         _anim.Play("Collect");
+//         yield return new WaitForSeconds(1f);
+//         Destroy(gameObject);
+//     }
+
+//     void OnTriggerEnter(Collider other)
+//     {
+//         if (other.tag == "Player")
+//         {
+//             StartCoroutine(CollectMail());
+//         }
+//     }
+// }
