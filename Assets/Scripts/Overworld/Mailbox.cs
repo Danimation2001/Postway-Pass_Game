fcac4913f26c2b5bd26994b35b4a43ffd02fd5b5
Daniel <dsquires.personal@outlook.com>
2022-05-30 00:05:26 +0800

    Implemented level end UI and Mailbox interaction.
    minimum mail must be declared on the mailbox prefab.

--- Assets/Scripts/Overworld/Mailbox.cs
+++ Assets/Scripts/Overworld/Mailbox.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Animations;
+using TMPro;
 
 public class Mailbox : MonoBehaviour
 {
+    public int minMail;
+    bool _canWin;
+
     public GameObject interactCanvas;
+    public GameObject popupUI;
+    public InputAction interact;
+    public TMP_Text mailCountText;
+    public TMP_Text goldMailCountText;
+    public TMP_Text canWinText;
+    public TMP_Text minimumText;
+
+    void OnEnable()
+    {
+        interact.Enable();
+        interact.performed += Interact;
+    }
+
+    void OnDisable()
+    {
+        interact.Disable();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            interact.Enable();
             interactCanvas.GetComponent<Animator>().Play("Fade In");
         }
     }
@@ -18,19 +42,63 @@ public class Mailbox : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            interact.Disable();
             interactCanvas.GetComponent<Animator>().Play("Fade Out");
         }
     }
-    
+
+    void Interact(InputAction.CallbackContext context)
+    {
+        if (!popupUI.activeSelf)
+        {
+            if (GameManager.Instance.mailCount >= minMail)
+            {
+                _canWin = true;
+            }
+
+            UpdateText();
+            popupUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void ClosePopup()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        popupUI.SetActive(false);
+    }
+
+    ConstraintSource _constraintSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        interact.Disable();
+        GameManager.Instance.CollectMaxMails();
 
+        _constraintSource.sourceTransform = Camera.main.transform;
+        _constraintSource.weight = 1;
+
+        GetComponentInChildren<LookAtConstraint>().AddSource(_constraintSource);
     }
 
     // Update is called once per frame
-    void Update()
+    void UpdateText()
     {
+        if (_canWin)
+        {
+            canWinText.text = "Collected Minimum: True";
+        }
+        else
+        {
+            canWinText.text = "Collected Minimum: False";
+        }
 
+        mailCountText.text = "Mail: " + GameManager.Instance.mailCount.ToString() + "/" + GameManager.Instance.maxMail.ToString();
+        goldMailCountText.text = "Gold Mail: " + GameManager.Instance.goldMailCount.ToString() + "/" + GameManager.Instance.maxGoldMail.ToString();
+
+        minimumText.text = "You need " + minMail + " pieces of mail to continue!";
     }
 }
