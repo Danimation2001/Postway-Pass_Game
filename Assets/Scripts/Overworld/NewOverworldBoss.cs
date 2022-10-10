caefc4b6384496bbb7aa26f5d3993046b3a94d43
Daniel <dsquires.personal@outlook.com>
2022-10-10 14:43:52 +0800

    Starting encounter test

---
+++ Assets/Scripts/Overworld/NewOverworldBoss.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewOverworldBoss : MonoBehaviour
+{
+    public int enemyID;
+    public GameObject combatPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (int id in GameManager.Instance.defeatedEnemies) // check if this enemy is marked as being defeated
+        {
+            if (id == enemyID)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.Instance.encounteredEnemy = enemyID;
+            GameManager.Instance.encounteredEnemyCombatPrefab = combatPrefab;
+            GameManager.Instance.lastPlayerPosition = other.transform.localPosition;
+            GameManager.Instance.lastPlayerRotation = other.transform.localRotation;
+            SceneLoader.Instance.LoadCombatScene();
+        }
+    }
+}
