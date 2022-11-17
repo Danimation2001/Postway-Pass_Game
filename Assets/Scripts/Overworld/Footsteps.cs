c0e9b8a10389cf6414b433796d38510baeb519c4
Kirsten <101615171+jellysnoms@users.noreply.github.com>
2022-11-17 16:34:17 +0800

    [SFX] added footsteps SFX

---
+++ Assets/Scripts/Overworld/Footsteps.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField] AudioClip[] clips;
+   
+   private void Awake()
+   {
+        audioSource = GetComponent<AudioSource>();
+   }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip, 1f);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}
