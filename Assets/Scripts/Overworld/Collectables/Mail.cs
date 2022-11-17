66d93fa8b7ee3cbd47c26b5dea95319dd2d408af
tillyjowle <101618668+tillyjowle@users.noreply.github.com>
2022-11-17 22:17:43 +0800

    [UI] Mail Popup

--- Assets/Scripts/Overworld/Collectables/Mail.cs
+++ Assets/Scripts/Overworld/Collectables/Mail.cs
@@ -7,6 +7,8 @@ public class Mail : MonoBehaviour
     public int mailID;
     Animator _anim;
 
+    public GameObject collectedMailPopUp;
+
     void Start()
     {
         foreach (int id in GameManager.Instance.collectedMail) // check if this mail is marked as been collected
@@ -23,16 +25,20 @@ public class Mail : MonoBehaviour
     {
         GameManager.Instance.mailCount++;
         GameManager.Instance.collectedMail.Add(mailID);
+        collectedMailPopUp.SetActive(true);
         _anim.Play("Collect");
         yield return new WaitForSeconds(1f);
         gameObject.SetActive(false);
+        collectedMailPopUp.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            //collectedMailPopUp.SetActive(true);
             StartCoroutine(CollectMail());
+            
         }
     }
 }
